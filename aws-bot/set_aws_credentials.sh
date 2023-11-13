1d6d75f91ec0a80f43e02f1d6195dfd0d5e33d3d
anonymous <anonymous@example.com>
2023-11-12 21:28:49 -0600

    Initial commit

---
+++ aws-bot/set_aws_credentials.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
+export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
+export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
